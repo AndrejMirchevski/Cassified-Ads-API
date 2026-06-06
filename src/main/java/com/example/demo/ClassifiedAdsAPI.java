d0af5a209dcf35e7f5f40c62fdefad6aeb8a06fb
AndrejMirchevski <amircevski2005@gmail.com>
2026-06-06 15:22:21 +0200

    fix: structure

--- src/main/java/com/example/demo/ClassifiedAdsAPI.java
+++ src/main/java/com/example/demo/ClassifiedAdsAPI.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class 	DemoApplication {
+public class  ClassifiedAdsAPI {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ClassifiedAdsAPI.class, args);
 	}
 
 }
